cae2ba3cdfc0ac7ff5099894cfab753805af07a9
dsusmit63 <dsusmit63@gmail.com>
2026-07-18 03:34:20 +0000

    added user_input.sh

---
+++ user_input.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Enter your firstname: " firstname
+
+read -p "Enter your lastname: " lastname
+
+read -p "Enter your age: " age
+
+echo "Hello $firstname $lastname.Your age is $age."
